4b7356c3bafa0da06a4e5c1b17ed5cf12ec21dec
snailwelligton <tarmo.rahmonen@gmail.com>
2018-11-25 21:21:14 +0200

    fixed distplot subset and updated the graphs with 25/11/2018 data

--- r/rent/002_rent_plots.R
+++ r/rent/002_rent_plots.R
@@ -96,7 +96,7 @@ tln_mean_price <- unnest_result %>%
 
 tln_mean_price
 
-ggplot(data = subset(unnest_result,date = max(unnest_result$date)), aes(x = total_price, fill = region))+
+ggplot(data = subset(unnest_result,date == max(unnest_result$date)), aes(x = total_price, fill = region))+
   geom_histogram(alpha = 0.5, bins = 30)+
   labs(x = paste("Price from ads on",max(unnest_result$date)),
        y = "Number of offers",
